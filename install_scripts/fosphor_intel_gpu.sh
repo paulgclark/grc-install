7fa8e2e04623953bd883d7d9215c5837c8f8e32f
Paul Clark <paulgclark@gmail.com>
2020-01-13 17:53:48 -0800

    Finally got a version working with grc 3.8. 3.7 still broken.

--- install_scripts/fosphor_intel_gpu.sh
+++ install_scripts/fosphor_intel_gpu.sh
@@ -120,11 +120,13 @@ else
 	# use a fosphor version compatible with cmake 2
 	FOSPHOR_REF="5d8751ee411fb93ec8434dd2d6e7341988a91cb5"
 fi
-#sudo apt -y install mesa-opencl-icd
 
-# after running the script, I also had to do the following with 3.8
-#sudo apt install beignet
-#sudo usermod -aG video $user_name
+# unfortunately, this flow doesn't work for older CPUs and gnuradio 3.7.x
+if [[ $GRC_38 == false ]] && [[ $BEIGNET == true ]]; then
+	echo "Sorry, this script will not work for older CPUs and GRC 3.7"
+	exit 1
+
+#sudo apt -y install mesa-opencl-icd
 
 # install glfw
 cd "$SRC_PATH" # custom block code lives at same level as gnuradio src
@@ -170,6 +172,7 @@ if [ $install_beignet == true ]; then
 	sudo apt -y install cmake pkg-config python ocl-icd-dev libegl1-mesa-dev \
 		ocl-icd-opencl-dev libdrm-dev libxfixes-dev libxext-dev \
 	       	clang-3.6 libtinfo-dev libedit-dev zlib1g-dev
+	sudo apt -y install llvm-3.9-dev libclang-3.9-dev
 	sudo apt -y install beignet
 	# removed llvm-3.6-dev and libclang-3.6-dev, not available via apt
 	# next install llvm and clang
@@ -183,6 +186,9 @@ if [ $install_beignet == true ]; then
 	# also add -DLLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi" ?
 	#sudo -u "$username" make
 	#sudo -u "$username" make install
+
+	# now install beignet from source
+	# 
 fi
 
 # now install gr-fosphor itself
@@ -202,7 +208,10 @@ sed -i '/FLG_FOSPHOR_USE_CLGL_SHARING/ s/1<<0/0<<0/' lib/fosphor/private.h
 
 sudo -u "$username" mkdir build
 cd build
-sudo -u "$username" cmake -DCMAKE_INSTALL_PREFIX=$TARGET_PATH ../
+#sudo -u "$username" cmake -DCMAKE_INSTALL_PREFIX=$TARGET_PATH ../
+sudo -u "$username" cmake -DCMAKE_INSTALL_PREFIX=$TARGET_PATH \
+                          -DCMAKE_MODULE_PATH=$TARGET_PATH/lib/cmake/gnuradio 
+                          -DCMAKE_BUILD_TYPE=RELEASE ../
 sudo -u "$username" make
 sudo -u "$username" make install
 
