1b8b2198a73960c9f01033cf4fc4fce4198ac2fb
Paul Clark <paulgclark@gmail.com>
2021-01-15 12:13:06 -0800

    updated fosphor for nvidia

--- install_scripts/fosphor/fosphor_nvidia_gpu.sh
+++ install_scripts/fosphor/fosphor_nvidia_gpu.sh
@@ -1,46 +1,37 @@
-#!/bin/bash
-# Best practice is to create an install directory off your home, then 
-# cloning the git repo as follows:
-# cd
-# mkdir install
-# cd install
-# git clone https://github.com/paulgclark/grc-install
-# 
-# NOTE: You should run one of the gnuradio installation scripts BEFORE
-# you run this one. Recommended is grc_from_source.sh
+#!/bin/bash -x
+# This script installs gr-gsm from source. It is intended for
+# execution AFTER the grc_from_source.sh script has been run and will
+# install to the same target directory used for that process.
 #
-# You should then run this script in place with the following commands:
-# cd grc-install/install-scripts
-# sudo ./grc_install_flabs_class.sh
-# 
-# If you run this script from another directory, you will break some
-# relative path links and the install will fail.
+# This script depends on environment variables that were created during
+# the gnuradio installation process, so the script must be run WITH
+# sudo:
+# sudo -E ./fosphor_nvidia_gpu.sh
 
-# get username
-username=$SUDO_USER
+# first get a few common functions:
+source ../common/common_functions.sh
 
-# get current directory (assuming the script is run from local dir)
-SCRIPT_PATH=$PWD
-cd ~/install
-INSTALL_PATH=`pwd`
-SRC_PATH=$INSTALL_PATH/src
-TARGET_PATH=$INSTALL_PATH/sdr
-cd $SCRIPT_PATH
+# call function to get globals: script_path, script_name, user_name, cores
+get_basic_info
+
+# check if running script as root, else exit
+exit_unless_root
+
+# check that the necessary environment vars in place, else exit
+check_env_vars
+
+glfw_repo=https://github.com/glfw/glfw
+glfw_ref=3.3.1
+fosphor_repo=git://git.osmocom.org/gr-fosphor
+fosphor_ref=defdd4aca6cd157ccc3b10ea16b5b4f552f34b96 
 
 # install dependencies
-sudo apt -y install cmake xorg-dev libglu1-mesa-dev swig3.0 qt4-default qtcreator python-qt4 swig wget
-#sudo apt -y install mesa-opencl-icd
+sudo apt -y install cmake 
+sudo apt -y install xorg-dev 
+sudo apt -y install libglu1-mesa-dev 
 
-# install glfw
-cd "$SRC_PATH" # custom block code lives at same level as gnuradio src
-# run git clone as user so we don't have root owned files in the system
-sudo -u "$username" git clone https://github.com/glfw/glfw
-cd glfw
-sudo -u "$username" mkdir build
-cd build
-sudo -u "$username" cmake -DCMAKE_INSTALL_PREFIX=$TARGET_PATH -DBUILD_SHARED_LIBS=true ../ 
-sudo -u "$username" make
-sudo -u "$username" make install
+# install glfw (needs extra cmake arg)
+clone_and_build $glfw_repo $glfw_ref sudo -DBUILD_SHARED_LIBS=true
 
 # final installation
 sudo apt -y install nvidia-opencl-dev
@@ -48,21 +39,29 @@ sudo apt -y install opencl-headers
 sudo apt -y install clinfo
 
 # now install gr-fosphor itself
-cd "$SRC_PATH" # custom block code lives at same level as gnuradio src
-sudo -u "$username" git clone git://git.osmocom.org/gr-fosphor
+cd $SDR_SRC_DIR # custom block code lives at same level as gnuradio src
+sudo -u "$username" git clone --recursive $fosphor_repo 
+cd gr-fosphor
+sudo -u "$username" git checkout $fosphor_ref
+sudo -u "$username" git submodule update
 
 # need to use sed/awk to modify the following file:
-# gr-fosphor/grc/fosphor_qt_sink_c.xml
-# $(gui_hint()($win))</make>  BECOMES $(gui_hint() % $win)</make>
-sed -i '/gui_hint()/ s/(\$win)/ % \$win/' gr-fosphor/grc/fosphor_qt_sink_c.xml
 # gr-fosphor/lib/fosphor/private.h
 # #define FLG_FOSPHOR_USE_CLGL_SHARING    (1<<0)  BECOMES  #define FLG_FOSPHOR_USE_CLGL_SHARING    (0<<0)
-sed -i '/FLG_FOSPHOR_USE_CLGL_SHARING/ s/1<<0/0<<0/' gr-fosphor/lib/fosphor/private.h
+sed -i '/FLG_FOSPHOR_USE_CLGL_SHARING/ s/1<<0/0<<0/' lib/fosphor/private.h
 
-cd gr-fosphor
 sudo -u "$username" mkdir build
 cd build
-sudo -u "$username" cmake -DCMAKE_INSTALL_PREFIX=$TARGET_PATH ../
-sudo -u "$username" make
+sudo -u "$username" cmake -DCMAKE_INSTALL_PREFIX=$SDR_TARGET_DIR ../
+sudo -u "$username" make $CORES
 sudo -u "$username" make install
- 
+
+
+echo "NOTE: to verify if gr-fosphor is working, please connect an SDR and"
+echo "      run the following flowgraph:"
+echo "      ~/install/grc-install/grc/fosphor/fosphor-example.grc"
+echo ""
+echo "      if foshphor does not work, please check that you have the"
+echo "      latest nVidia drivers installed"
+echo ""
+echo "      for more details, please see www.factorialabs.com"
