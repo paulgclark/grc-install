c7d3f4eeb5bed41e6b5f5dff6534d13e14cd83c3
Paul Clark <paulgclark@gmail.com>
2021-01-14 19:40:40 -0800

    added install script for brave browser

---
+++ misc/brave_from_cli.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+#
+# The following commands are taken nearly verbatim from:
+# https://brave.com/linux/#linux
+
+sudo apt install -y apt-transport-https curl gnupg
+
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt -y install brave-browser
