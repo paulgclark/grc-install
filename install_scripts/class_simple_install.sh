9550d51d4934962f938de17eda829f7b4c189717
Paul Clark <paulgclark@gmail.com>
2022-01-22 20:11:32 -0800

    added oot dependencies

--- install_scripts/class_simple_install.sh
+++ install_scripts/class_simple_install.sh
@@ -18,28 +18,32 @@ sudo apt -y install git
 sudo apt -y install cmake
 sudo apt -y install swig
 
+# pre-reqs
+sudo apt -y install liborc-0.4-dev
+sudo apt -y install feh
+
 # install gr-reveng custom block
 cd $INSTALL_PATH  
-mkdir -p src
+sudo -u $username mkdir -p src
 # run git clone as user so we don't have root owned files in the user space
-sudo -u "$username" git clone https://github.com/paulgclark/gr-reveng
+sudo -u $username git clone https://github.com/paulgclark/gr-reveng
 cd gr-reveng
-sudo -u "$username" mkdir build
+sudo -u $username mkdir build
 cd build
-sudo -u "$username" cmake ../
-sudo -u "$username" make
+sudo -u $username cmake ../
+sudo -u $username make
 sudo make install
 sudo ldconfig
 
 # install gr-satellites custom block
 cd $INSTALL_PATH/src 
 # run git clone as user so we don't have root owned files in the user space
-sudo -u "$username" git clone https://github.com/daniestevez/gr-satellites
+sudo -u $username git clone https://github.com/daniestevez/gr-satellites
 cd gr-satellites
-sudo -u "$username" mkdir build
+sudo -u $username mkdir build
 cd build
-sudo -u "$username" cmake ../
-sudo -u "$username" make
+sudo -u $username cmake ../
+sudo -u $username make
 sudo make install
 sudo ldconfig
 
@@ -48,11 +52,11 @@ sudo snap install pycharm-community --classic
 
 # add Python code
 cd $INSTALL_PATH/src 
-sudo -u "$username" git clone https://github.com/paulgclark/rf_utilities
+sudo -u $username git clone https://github.com/paulgclark/rf_utilities
 # add bashrc path
-sudo -u "$username" echo "" >> ~/.bashrc
-sudo -u "$username" echo "################################" >> ~/.bashrc
-sudo -u "$username" echo "# Custom code for gnuradio class" >> ~/.bashrc
-sudo -u "$username" echo "export PYTHONPATH=\$PYTHONPATH:$INSTALL_PATH/src/rf_utilities"  >> ~/.bashrc
-sudo -u "$username" echo "" >> ~/.bashrc
+sudo -u $username echo "" >> ~/.bashrc
+sudo -u $username echo "################################" >> ~/.bashrc
+sudo -u $username echo "# Custom code for gnuradio class" >> ~/.bashrc
+sudo -u $username echo "export PYTHONPATH=\$PYTHONPATH:$INSTALL_PATH/src/rf_utilities"  >> ~/.bashrc
+sudo -u $username echo "" >> ~/.bashrc
 
